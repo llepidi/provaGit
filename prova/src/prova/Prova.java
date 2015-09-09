e5c645585a17e50971edb3263be1369a5b873928
Luciano Lepidi <luciano.lepidi@sinapsi.com>
2015-09-09 11:18:08 +0200

    Created Test class.

--- prova/src/prova/Prova.java
+++ prova/src/prova/Prova.java
@@ -5,12 +5,7 @@ public class Prova {
 	public static void main(String[] args) {
 		String s = new String("2");
 		String x = new String("5");
-		if(s==x){
-			System.out.println("Eq");
-		}else{
-			System.out.println("NEq");
-		}
-
+		new Test().doTest(x, s);
 	}
 
 }
---
+++ prova/src/prova/Test.java
@@ -0,0 +1,11 @@
+package prova;
+
+public class Test {
+	public void doTest(String x, String s) {
+		if (s == x) {
+			System.out.println("Eq");
+		} else {
+			System.out.println("NEq");
+		}
+	}
+}
