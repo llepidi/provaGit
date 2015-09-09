baa5ad5c4aad59ac8e00eccb25b8f5c7f961401d
Luciano Lepidi <luciano.lepidi@sinapsi.com>
2015-09-09 11:20:48 +0200

    Created Test2.

---
+++ prova/src/prova/Test2.java
@@ -0,0 +1,11 @@
+package prova;
+
+public class Test2 {
+	public void doTest(String x, String s) {
+		if (s == x) {
+			System.out.println("Eq");
+		} else {
+			System.out.println("NEq");
+		}
+	}
+}
