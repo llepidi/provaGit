49aed003507fee7033c87307c8e2b7dce6a59cf1
Luciano Lepidi <luciano.lepidi@sinapsi.com>
2015-09-09 11:07:47 +0200

    Initial commit

---
+++ prova/src/prova/Prova.java
@@ -0,0 +1,16 @@
+package prova;
+
+public class Prova {
+
+	public static void main(String[] args) {
+		String s = new String("2");
+		String x = new String("5");
+		if(s==x){
+			System.out.println("Eq");
+		}else{
+			System.out.println("NEq");
+		}
+
+	}
+
+}
