fc079d14a1701fe20c6f46c2cf3775a538a9ed58
Luciano Lepidi <luciano.lepidi@sinapsi.com>
2015-10-16 14:50:44 +0200

    Migliorie

--- prova/src/prova/Test2.java
+++ prova/src/prova/Test2.java
@@ -3,9 +3,9 @@ package prova;
 public class Test2 {
 	public void doTest(String x, String s) {
 		if (s.equals(x)) {
-			System.out.println("2 Eq");
+			System.out.println("2 Equals");
 		} else {
-			System.out.println("2 Not Eq");
+			System.out.println("2 Not Equals");
 		}
 	}
 }
